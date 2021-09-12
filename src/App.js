09c72dbe6c20ecf6e614958fcfaa24e1a0ed7b95
Caleb Phillips <csphillips5@gmail.com>
2021-09-12 00:05:58 -0400

    5-2 create axios POST req to login, implement handleLogin logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Note from "./components/Note";
 import Notification from "./components/Notification";
 import Footer from "./components/Footer";
 import noteService from "./services/notes";
+import loginService from "./services/login";
 
 const App = (props) => {
   const [notes, setNotes] = useState([]);
@@ -11,13 +12,12 @@ const App = (props) => {
   const [errorMessage, setErrorMessage] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    noteService
-      .getAll()
-      .then((initialNotes) => {
-        setNotes(initialNotes);
-      });
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
+    });
   }, []);
 
   const addNote = (event) => {
@@ -28,13 +28,11 @@ const App = (props) => {
       important: Math.random() > 0.5,
     };
 
-    noteService
-      .create(noteObject)
-      .then((returnedNote) => {
-        console.log('create note returned note:', returnedNote);
-        setNotes(notes.concat(returnedNote));
-        setNewNote("");
-      });
+    noteService.create(noteObject).then((returnedNote) => {
+      console.log("create note returned note:", returnedNote);
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -56,17 +54,65 @@ const App = (props) => {
       .catch((error) => {
         setErrorMessage(
           `Note "${noteToToggle.content}" was already removed from server`
-        )
+        );
         setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
+          setErrorMessage(null);
+        }, 5000);
       });
   };
 
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault();
-    console.log('logging in with', username, password);
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
   };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input
+        value={newNote}
+        onChange={handleNoteChange}
+      />
+      <button type="submit">save</button>
+    </form>
+  )
 
   return (
     <div>
@@ -76,19 +122,19 @@ const App = (props) => {
         <div>
           username
           <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
           />
         </div>
         <div>
           password
           <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
           />
         </div>
         <button type="submit">login</button>
---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const baseURL = "/api/login";
+
+const login = async (credentials) => {
+  const response = await axios.post(baseURL, credentials);
+  return response.data;
+};
+
+const loginRequest = { login };
+
+export default loginRequest;
